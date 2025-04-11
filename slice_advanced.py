baaf5a4c73ac84a304669f92230119e786e28faf
ignaciopiccolo <igpiccolovazquez@mail.austral.edu.ar>
2025-04-11 10:26:28 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,4 @@
 def slice_advanced():
-    # Código a implementar utilizando input.
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+    texto = input("> ")
+    resultado = texto[4::2]  # Empezar desde el 5° carácter (índice 4) y tomar cada 2 caracteres
+    print(resultado)
